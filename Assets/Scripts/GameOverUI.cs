b71c63c25f4e73afd1771d5c74cf5c9faff69319
devdixittt <devdixit9719538900@gmail.com>
2025-02-13 13:06:13 +0530

    Initial commit

---
+++ Assets/Scripts/GameOverUI.cs
@@ -0,0 +1,49 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverUI : MonoBehaviour {
+
+
+    [SerializeField] private TextMeshProUGUI resultTextMesh;
+    [SerializeField] private Color winColor;
+    [SerializeField] private Color loseColor;
+    [SerializeField] private Color tieColor;
+
+
+    private void Awake() {
+    
+    }
+    private void Start() {
+        GameManager.Instance.OnGameWin += GameManager_OnGameWin;
+        GameManager.Instance.OnGameTied += GameManager_OnGameTied;
+
+        Hide();
+    }
+
+    private void GameManager_OnGameTied(object sender, System.EventArgs e) {
+        resultTextMesh.text = "TIE!";
+        resultTextMesh.color = tieColor;
+        Show();
+    }
+
+
+    private void GameManager_OnGameWin(object sender, GameManager.OnGameWinEventArgs e) {
+        if (e.winPlayerType == GameManager.Instance.GetLocalPlayerType()) {
+            resultTextMesh.text = "YOU WIN!";
+            resultTextMesh.color = winColor;
+        } else {
+            resultTextMesh.text = "YOU LOSE!";
+            resultTextMesh.color = loseColor;
+        }
+        Show();
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+}
